0e4df468066d4fce3d9dcf648599573415863157
Satish Rathod <satishrathod.dev@gmail.com>
2024-11-01 19:27:07 +0530

    finsihed

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import { Route, Routes } from "react-router-dom";
 const App = () => {
   const users = [
     { id: 1, name: "Leanne Graham", email: "leanne@example.com" },
-    { id: 2, name: "Bret Rathod", email: "bret@example.com" },
+    { id: 2, name: "Bret", email: "bret@example.com" },
     { id: 3, name: "Shubham Wagh", email: "shubham@example.com" },
     { id: 4, name: "Rohit Palkar", email: "rohit@example.com" },
   ];
